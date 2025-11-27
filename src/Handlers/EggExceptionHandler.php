1c4a73a6ec0e73033793feba7d513dfea33a2a1e
Cowio <Kasper.gorm@gmail.com>
2025-11-27 11:14:57 +0100

    Update exception reporting endpoint to localhost
    Changed the HTTP POST request URL in EggExceptionHandler from 'eggplatform' to 'localhost' for local exception reporting. This ensures exceptions are sent to the correct local API endpoint during development.

--- src/Handlers/EggExceptionHandler.php
+++ src/Handlers/EggExceptionHandler.php
@@ -15,7 +15,7 @@ class EggExceptionHandler extends ExceptionHandler
         dump("EggExceptionHandler: Reporting exception - " . $e->getMessage());
 
         // HTTP POST request using Laravel's HTTP client, including exception details in the body
-        $response = Http::post('http://eggplatform:8080/api/exception', [
+        $response = Http::post('http://localhost:8080/api/exception', [
             'message' => $e->getMessage(),
             'exception_class' => get_class($e),
             'code' => $e->getCode(),
